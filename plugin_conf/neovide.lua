4ee13e9128bd8545bc0cef076edb61ef1b7473ae
Andy3153 <andy3153@protonmail.com>
2022-05-26 22:27:40 +0300

    changed neovide cursor effect

--- plugin_conf/neovide.lua
+++ plugin_conf/neovide.lua
@@ -5,10 +5,10 @@
 local g =   vim.g
 local key = vim.keymap.set
 
-g.neovide_transparency=0.8              -- transparency
-g.neovide_cursor_animation_length=0.05  -- duration of cursor animation
---g.neovide_cursor_vfx_mode = "ripple"  -- cursor effect
-g.neovide_cursor_vfx_mode = "railgun"   -- cursor effect
+g.neovide_transparency = 0.8              -- transparency
+g.neovide_cursor_animation_length = 0.05  -- duration of cursor animation
+g.neovide_cursor_vfx_mode = "ripple"    -- cursor effect
+--g.neovide_cursor_vfx_mode = "torpedo" -- cursor effect
 
 
 -- Make Ctrl-+ and Ctrl-- work like in a terminal
