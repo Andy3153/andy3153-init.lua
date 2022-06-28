0129c3cf8fd9b284b0a2ce4215c8b3b31ccddf82
Andy3153 <andy3153@protonmail.com>
2022-06-29 02:09:03 +0300

    customized start screen even more

--- lua/plugin_conf/alpha.lua
+++ lua/plugin_conf/alpha.lua
@@ -75,9 +75,11 @@ theme.section.header.val = ascii_bloody  -- Header
 
 theme.section.buttons.val =                       -- Menu
 {
-  theme.button('e',       ' New file',        ':enew<CR>'),
-  theme.button('SPC SPC', ' Recent files',    ':Telescope oldfiles<CR>'),
-  theme.button('cfv',     ' Neovim settings', ':e $MYVIMRC<CR>'),
-  theme.button('cfz',     ' Zsh settings',    ':e $ZDOTDIR/.zshrc<CR>'),
-  theme.button('q',       ' Quit Neovim',     ':qall<CR>'),
+  theme.button('e',       '  New file',        ':enew<CR>'),
+  theme.button('SPC SPC', '  Recent files',    ':Telescope oldfiles<CR>'),
+  theme.button('SPC r',   '  Ranger',          ':RnvimrToggle<CR>'),
+  theme.button('SPC t',   '  FTerm',           ':lua require("FTerm").toggle()<CR>'),
+  theme.button('cfv',     '  Neovim settings', ':e $MYVIMRC<CR>'),
+  theme.button('cfz',     '  Zsh settings',    ':e $ZDOTDIR/.zshrc<CR>'),
+  theme.button('q',       '  Quit Neovim',     ':qall<CR>'),
 }
