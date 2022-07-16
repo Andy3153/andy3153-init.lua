254ed4c9f3cf6970d2f5146c710286a649a79e30
Andy3153 <andy3153@protonmail.com>
2022-07-16 15:30:26 +0300

    plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,6 @@
 -- vim: set fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
@@ -18,7 +18,7 @@ return require('packer').startup(function(use)
     use
     {
       'nvim-lualine/lualine.nvim',                                -- Statusline
-      requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+      requires = { 'kyazdani42/nvim-web-devicons' },
       config   = function() require('plugin_conf/lualine') end
     }
 
