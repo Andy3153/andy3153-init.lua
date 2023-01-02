3db1f13d19818d5f14b95142583fa50d004fdaed
Andy3153 <andy3153@protonmail.com>
2023-01-02 17:42:23 +0200

    neovide: changed default refresh rate

--- lua/plugin_conf/neovide.lua
+++ lua/plugin_conf/neovide.lua
@@ -3,13 +3,15 @@
 --
 
 g.neovide_transparency = 0.8              -- transparency
-g.neovide_cursor_animation_length = 0.05  -- duration of cursor animation
-g.neovide_cursor_vfx_mode = "ripple"    -- cursor effect
---g.neovide_cursor_vfx_mode = "torpedo" -- cursor effect
+g.neovide_cursor_animation_length = 0.08  -- duration of cursor animation
+g.neovide_cursor_vfx_mode = "ripple"      -- cursor effect
 
+-- Refresh rate
+g.neovide_refresh_rate = 240
+g.neovide_refresh_rate_idle = 5
 
 -- Make Ctrl-+ and Ctrl-- work like in a terminal
-g.gui_font_default_size = 8.05          -- default font size
+g.gui_font_default_size = 8.05            -- default font size
 g.gui_font_size = g.gui_font_default_size
 g.gui_font_face = "Iosevka Nerd Font Mono"
 
