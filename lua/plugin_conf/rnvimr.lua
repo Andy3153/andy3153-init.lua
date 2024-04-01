6a227e4f98ec7d6acfe0d698765bd07ac88ff90f
Andy3153 <andy3153@protonmail.com>
2024-04-01 19:31:41 +0300

    plugin/rnvimr: trying to switch away from rnvimr

--- lua/plugin_conf/rnvimr.lua
+++ lua/plugin_conf/rnvimr.lua
@@ -1,6 +1,7 @@
+-- vim: set fenc=utf-8 ts=2 sw=0 sts=0 sr et si tw=0 fdm=marker fmr={{{,}}}:
 --
 -- rnvimr configuration
 --
 
-g.rnvimr_enable_ex = 1     -- use rnvimr instead of nvim filemanager
+--g.rnvimr_enable_ex = 1     -- use rnvimr instead of nvim filemanager
 g.rnvimr_enable_picker = 1 -- hide after opening file
