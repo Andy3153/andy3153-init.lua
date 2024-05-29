a62baab0cff2cae0008a68313f153503a76c2091
Andy3153 <andy3153@protonmail.com>
2024-05-29 03:45:45 +0300

    plugin_conf/mason: prioritize system lsps over downloaded ones (good on nixos)

--- lua/plugin_conf/mason.lua
+++ lua/plugin_conf/mason.lua
@@ -6,4 +6,5 @@ require("mason").setup(
 {
   automatic_installation = true,            -- Whether servers that are set up should be automatically installed if they're not already installed.
   max_concurrent_installers = 5,            -- Limit for simultaneous installs
+  PATH = "append",                          -- Use system packages when available (useful for NixOS)
 })
