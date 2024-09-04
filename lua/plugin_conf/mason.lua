2cbc6f6d33452948a8d842bce23ac7dd8dbf9e0c
Andy3153 <andy3153@protonmail.com>
2024-09-04 12:54:58 +0300

    plugins/mason: cleanup

--- lua/plugin_conf/mason.lua
+++ lua/plugin_conf/mason.lua
@@ -1,10 +1,9 @@
 --
--- mason configuration
+-- Mason configuration
 --
 
 require("mason").setup(
 {
-  automatic_installation = true,            -- Whether servers that are set up should be automatically installed if they're not already installed.
-  max_concurrent_installers = 5,            -- Limit for simultaneous installs
-  PATH = "append",                          -- Use system packages when available (useful for NixOS)
+  max_concurrent_installers = 5, -- Limit for simultaneous installs
+  PATH = "append",               -- Use system packages when available (useful for NixOS)
 })
