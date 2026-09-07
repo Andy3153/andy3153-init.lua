d4ae086c9fc6a01cf2a7f4efceff9dfe94c7e49c
Andy3153 <andy3153@protonmail.com>
2026-09-07 16:02:33 +0300

    plugins/neovide: rewrite

--- lua/plugin_conf/neovide.lua
+++ lua/plugin_conf/neovide.lua
@@ -2,9 +2,42 @@
 --
 -- Neovide configuration
 --
+-- https://neovide.dev/configuration.html
+--
 
-g.neovide_opacity                 = 0.8    -- transparency
-g.neovide_cursor_animation_length = 0.08   -- duration of cursor animation
-g.neovide_cursor_vfx_mode = "pixiedust"    -- cursor effect
-g.neovide_cursor_vfx_particle_lifetime = 1 -- particle lifetime
+-- {{{ Variables
+local padding = 5
+local opacity = .8
+-- }}}
+
+-- {{{ Display
+-- {{{ Padding
+g.neovide_padding_top    = padding
+g.neovide_padding_bottom = padding
+g.neovide_padding_left   = padding
+g.neovide_padding_right  = padding
+-- }}}
+
+-- {{{ Transparency
+g.neovide_opacity        = opacity
+g.neovide_normal_opacity = opacity
+-- }}}
+
+-- {{{ Progress bar
+g.neovide_progress_bar_height          = padding
+g.neovide_progress_bar_animation_speed = 400
+g.neovide_progress_bar_hide_delay      = .15
+-- }}}
+-- }}}
+
+-- {{{ Cursor settings
+g.neovide_cursor_animation_length       = .15
+g.neovide_cursor_short_animation_length = .05
+-- }}}
+
+-- {{{ Cursor particles
+g.neovide_cursor_vfx_mode          = { "pixiedust", "ripple" }
+g.neovide_cursor_vfx_particle_lifetime = .75
+g.neovide_cursor_vfx_particle_highlight_lifetime = .175
 g.neovide_cursor_vfx_particle_density = 2  -- particle density
+-- }}}
